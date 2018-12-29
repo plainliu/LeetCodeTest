1f5e28c0eb64480ceb5dcaca58d429b16cc8edd1
Plain <liujuanjuan134@foxmail.com>
2018-12-29 14:02:46 +0800

    Update 11 Container With Most Water.cpp

--- LeetCodeCppTest/11 Container With Most Water.cpp
+++ LeetCodeCppTest/11 Container With Most Water.cpp
@@ -1,43 +1,18 @@
 /*
- *3.27
+ *2018.12.29
  *PlainLiu
- *
- *
  */
- 
-//1.0 time out
+//Approach 1: Brute Force
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int max=0;
-        int x=0,h=0;//x*h
-        for(int i=0;i<height.size()-1;++i){
-            x=1;
-            while(i+x<height.size()){
-                h=(height[i]<=height[i+x]?height[i]:height[i+x]);
-                max=(h*x>max?h*x:max);
-                x++;
+        int max_water=0,cur_water=0,num=height.size();
+        for(int i=0,j=1;i<num-1;++i){
+            for(j=i+1;j<num;++j){
+                cur_water=min(height[i],height[j])*(j-i);
+                if(cur_water>max_water)max_water=cur_water;
             }
         }
-        return max;
-    }
-};
-
-//2.0 time out
-class Solution {
-public:
-    int maxArea(vector<int>& height) {
-        int i=0,h=0;//x*h
-        int max=(height.size()-1)*(height[0]<height[height.size()-1]?height[0]:height[height.size()-1]);
-        for(int x=height.size()-2;x>0;--x){
-            i=0;
-            while(i+x<height.size()){
-                h=(height[i]<height[i+x]?height[i]:height[i+x]);
-                i++;
-                if(h<max/x)continue;
-                max=(h*x>max?h*x:max);
-            }
-        }
-        return max;
+        return max_water;
     }
 };

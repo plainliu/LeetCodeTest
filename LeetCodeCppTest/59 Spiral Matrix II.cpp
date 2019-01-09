2326cb7324d5560c0cdcc3c119f1c83cb578b495
Plain <liujuanjuan134@foxmail.com>
2019-01-09 23:47:36 +0800

    Create 59 Spiral Matrix II.cpp

---
+++ LeetCodeCppTest/59 Spiral Matrix II.cpp
@@ -0,0 +1,25 @@
+
+//wrong
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<int> cur(n);
+        vector<vector<int>> res(n,cur);
+        int s=n*n;
+        for(int i=0,j=0,k=1;k<=s;){
+            while(k<=s&&j<n&&res[i][j]==0)//right
+                res[i][j++]=k++;
+            --j;
+            while(i<n&&res[i][j]==0)//down
+                res[i++][j]=k++;
+            --i;
+            while(j>=0&&res[i][j]==0)//left
+                res[i][j--]=k++;
+            ++j;
+            while(res[i][j]==0)//up
+                res[i--][j]=k++;
+            ++i;
+        }
+        return res;
+    }
+};
